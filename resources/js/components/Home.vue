28bbc347f93b222a6e1d3a9fc8c05968893cec72
keithchasenm <keithchasen89@gmail.com>
2018-12-17 17:13:58 +0200

    basic set up vue app

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        HOME
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="main">
+        VUE APP
+        <div class="content">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'main-app'
+    }
+</script>
