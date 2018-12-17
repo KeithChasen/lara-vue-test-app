4872dddf9b7853ecdc4cad9dc0cfe8e4db2eabca
keithchasenm <keithchasen89@gmail.com>
2018-12-17 18:38:26 +0200

    basic store functionality

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,8 +1,51 @@
+import {getLocalUser} from "./auth";
 
+const user = getLocalUser();
 
 export default {
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {}
+    state: {
+        currentUser: user,
+        isLoggedIn: !!user,
+        loading: false,
+        auth_error: null,
+        users: [],
+        photos: []
+    },
+    getters: {
+        isLoggedIn: state => state.isLoggedIn,
+        currentUser: state => state.currentUser,
+        authError: state => state.auth_error,
+        users: state => state.users,
+        photos: state => state.photos,
+    },
+    mutations: {
+        login: state => {
+            state.loading = true;
+            state.auth_error = null;
+        },
+        loginSuccess: (state, payload) => {
+            state.auth_error = null;
+            state.isLoggedIn = true;
+            state.loading = false;
+            state.currentUser = Object.assign(
+                {},
+                payload.user,
+                {token: payload.access_token}
+            );
+
+            localStorage.setItem('user', JSON.stringify(state.currentUser));
+        },
+        loginFailed: (state, payload) => {
+            state.loading = false;
+            state.auth_error = payload.error;
+        },
+        logout: state => {
+            localStorage.removeItem('user');
+            state.isLoggedIn = false;
+            state.currentUser = null;
+        }
+    },
+    actions: {
+        login: context => context.commit('login')
+    }
 }
