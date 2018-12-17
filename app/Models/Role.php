1ffa40c896e97ca3ec58cfe0b3ef3c579564667d
keithchasenm <keithchasen89@gmail.com>
2018-12-17 15:53:11 +0200

    user roles

---
+++ app/Models/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    const ADMIN_ROLE = 'admin';
+    const USER_ROLE = 'user';
+
+    public static $roles = [
+        self::ADMIN_ROLE,
+        self::USER_ROLE
+    ];
+
+    protected $fillable = [
+        'role'
+    ];
+}
